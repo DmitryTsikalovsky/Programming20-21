7367f1cd3648a8d1eea614e0113c7959878508bb
DmitryTsikalovsky <dimitry.tsikalovsky@gmail.com>
2021-11-26 18:16:33 +0300

    add genarray

--- lab_5/main.cpp
+++ lab_5/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <chrono>
 #include <cmath>
+#include <fstream>
 
 class Timer {
     std::chrono::time_point<std::chrono::system_clock> m_StartTime;
@@ -40,20 +41,44 @@ bool searchPeopleByName(const People& data, const std::string& key){
     if(data.name == key){return true;}else{return false;}
 }
 
+DynamicArray<People, std::string>& genRandomPeople(int n){
+    DynamicArray<People, std::string> randomArray(n);
+    auto *line = new std::string[617];
+
+    std::ifstream in("/home/dima/CLionProjects/git/Programming20-21/lab_5/Names.txt"); // окрываем файл для чтения
+    if (in.is_open())
+    {
+        int i = 0;
+        while (getline(in, line[i])){
+//            std::cout<< line[i]<<endl;
+            i++;
+        }
+
+    }
+    in.close();
+    People insertPeople;
+    for (int i = 0; i < n; ++i) {
+        insertPeople.name = line[rand() % 617];
+        insertPeople.number = rand() % 100;
+        randomArray.append(insertPeople);
+    }
+
+}
+
 
 int main(){
 
     //Проверка хеш таблицы
 //    HashTable<People, std::string> testTable(10,&hashFunction);
-    People John;
-    People Jame;
-    People David;
-    John.name = "John";
-    John.number = 1234567;
-    Jame.name = "Jame";
-    Jame.number = 7654321;
-    David.name = "David";
-    David.number = 89101112;
+//    People John;
+//    People Jame;
+//    People David;
+//    John.name = "John";
+//    John.number = 1234567;
+//    Jame.name = "Jame";
+//    Jame.number = 7654321;
+//    David.name = "David";
+//    David.number = 89101112;
 
 //    testTable.add(John.name, John);
 //    testTable.add(Jame.name, Jame);
@@ -72,10 +97,10 @@ int main(){
 //        testQueue.remove();
 //    }
     //Проверка динамик аррея
-    DynamicArray<People, std::string> testArray;
-    testArray.append(Jame);
-    testArray.append(John);
-    testArray.append(David);
+//    DynamicArray<People, std::string> testArray;
+//    testArray.append(Jame);
+//    testArray.append(John);
+//    testArray.append(David);
 //    for (int i = 0; i < 3; ++i) {
 //        cout<< testArray[i].number << " ";
 //    }
@@ -83,19 +108,22 @@ int main(){
 //    People result = testArray.search(Jame.name,searchPeopleByName);
 //    cout<< result.name << " ";
 
-    Cache<std::string, People> testCache(testArray,hashFunction, 3, 5000);
-    People Georgy;
-    Georgy.name = "Georgy";
-    Georgy.number = 1991;
-    testCache.add(Georgy);
-    testCache.add(Georgy);
+//    Cache<std::string, People> testCache(testArray,hashFunction, 3, 5000);
+//    People Georgy;
+//    Georgy.name = "Georgy";
+//    Georgy.number = 1991;
+//    testCache.add(Georgy);
+//    testCache.add(Georgy);
+//
+//    Timer timer;
+//    timer.Start();
+//    cout<< testCache.get(Georgy.name, &searchPeopleByName).name << " " << timer.GetDuration() << endl;
+//    timer.Start();
+//    cout<< testCache.get(Georgy.name, &searchPeopleByName).name << " " << timer.GetDuration()<< endl;
 
-    Timer timer;
-    timer.Start();
-    cout<< testCache.get(Georgy.name, &searchPeopleByName).name << " " << timer.GetDuration() << endl;
-    timer.Start();
-    cout<< testCache.get(Georgy.name, &searchPeopleByName).name << " " << timer.GetDuration()<< endl;
 
+// Тест генерации рандомных имен
+    genRandomPeople(10);
 
 
 
