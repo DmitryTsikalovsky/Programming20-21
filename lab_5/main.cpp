7a89a0dce3f701392bdaeb90eaa8079884509615
DmitryTsikalovsky <dimitry.tsikalovsky@gmail.com>
2021-12-01 00:03:43 +0300

    do part of the task

--- lab_5/main.cpp
+++ lab_5/main.cpp
@@ -6,7 +6,10 @@
 #include <string>
 #include <chrono>
 #include <cmath>
+#include <algorithm>
 #include <fstream>
+#include <vector>
+#include <sstream>
 
 class Timer {
     std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTime;
@@ -75,24 +78,45 @@ People *genRandomPeople(int n){
     return insertPeople;
 }
 
+void createArrayOfWords(){
+    std::vector<std::string> words;
+    std::string line;
+    std::string word;
+    std::ifstream in("Names.txt"); // окрываем файл для чтения
+    if (in.is_open())
+    {
+        while (std::getline(in, line, ' ')){
+            words.push_back(line);
+        }
+
+    }
+
+    for (int i = 0; i < 100; ++i) {
+        std::cout<<words[i]<<std::endl;
+    }
+    in.close();
+    return;
+}
+
 
 int main(){
+    createArrayOfWords();
 
-    DynamicArray<People> bigData(genRandomPeople(10000), 10000);
-    for (int i = 0; i < 10000; ++i) {
-        cout<< bigData[i].name << " "<< endl;
-    }
-    Cache<People, int> theBestCacheInTheWorld(idHashFunction, 100,10000);
-
-    for (int i = 0; i < 999; ++i) {
-        theBestCacheInTheWorld.add(bigData[i], bigData[i].id);
-    }
-
-    for (int i = 0; i < 1000; ++i) {
-        if (theBestCacheInTheWorld.search(bigData[i].id)){
-            cout<< theBestCacheInTheWorld.get(bigData[i].id).id<<  " "<<theBestCacheInTheWorld.get(bigData[i].id).name << endl;
-        }
-    }
+//    DynamicArray<People> bigData(genRandomPeople(10000), 10000);
+//    for (int i = 0; i < 10000; ++i) {
+//        cout<< bigData[i].name << " "<< endl;
+//    }
+//    Cache<People, int> theBestCacheInTheWorld(idHashFunction, 100,10000);
+//
+//    for (int i = 0; i < 999; ++i) {
+//        theBestCacheInTheWorld.add(bigData[i], bigData[i].id);
+//    }
+//
+//    for (int i = 0; i < 1000; ++i) {
+//        if (theBestCacheInTheWorld.search(bigData[i].id)){
+//            cout<< theBestCacheInTheWorld.get(bigData[i].id).id<<  " "<<theBestCacheInTheWorld.get(bigData[i].id).name << endl;
+//        }
+//    }
 //    std::cout<< endl;
 //    std::cout<< theBestCacheInTheWorld.get(bigData[0].id).name << " "<< endl;
 
